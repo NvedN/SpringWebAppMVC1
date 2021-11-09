82dbc09b7cd6f10d5bce8e4d857b6a4fde27b0c7
NvedN <psp3000rus@Mail.ru>
2021-11-09 08:28:51 +0300

    javax validation

--- src/main/java/ru/nvn/spring/controllers/PeopleController.java
+++ src/main/java/ru/nvn/spring/controllers/PeopleController.java
@@ -1,6 +1,5 @@
 package ru.nvn.spring.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +7,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.nvn.spring.dao.PersonDAO;
 import ru.nvn.spring.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
