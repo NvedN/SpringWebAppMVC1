b2cbd8d2690df9d4b95372f4cb155f6f593fd364
Nikita <35899629+NvedN@users.noreply.github.com>
2022-03-01 15:02:58 +0300

    upload files

--- src/main/java/ru/nvn/spring/config/SpringConfig.java
+++ src/main/java/ru/nvn/spring/config/SpringConfig.java
@@ -7,15 +7,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
 
 /**
  * @author NVN
@@ -23,55 +26,110 @@ import java.sql.DriverManager;
 @Configuration
 @ComponentScan("ru.nvn.spring")
 @EnableWebMvc
-public class SpringConfig implements WebMvcConfigurer {
+public class SpringConfig implements WebMvcConfigurer
+{
 
-    private final ApplicationContext applicationContext;
+		private final ApplicationContext applicationContext;
+//
+		@Autowired
+		public SpringConfig(ApplicationContext applicationContext)
+		{
+				this.applicationContext = applicationContext;
+		}
 
-    @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
+//		@Bean
+//		public SpringResourceTemplateResolver templateResolver()
+//		{
+//				SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+//				templateResolver.setApplicationContext(applicationContext);
+//				templateResolver.setPrefix("/WEB-INF/views/");
+//				templateResolver.setSuffix(".html");
+//				return templateResolver;
+//		}
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
-    }
+//		@Bean
+//		public SpringTemplateEngine templateEngine()
+//		{
+//				SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//				templateEngine.setTemplateResolver(templateResolver());
+//				templateEngine.setEnableSpringELCompiler(true);
+//				return templateEngine;
+//		}
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
+//		@Autowired
+//		WebApplicationContext webApplicationContext;
+//
+		@Bean
+		public SpringResourceTemplateResolver templateResolver(){
+				SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+				templateResolver.setApplicationContext(applicationContext);
+				templateResolver.setPrefix("/WEB-INF/views/");
+				templateResolver.setSuffix("");
+				return templateResolver;
+		}
+//
+		@Bean
+		public SpringTemplateEngine templateEngine() {
+				SpringTemplateEngine springTemplateEngine= new SpringTemplateEngine();
+				springTemplateEngine.setTemplateResolver(templateResolver());
+				springTemplateEngine.setEnableSpringELCompiler(true);
+				return springTemplateEngine;
+		}
+//
+		@Bean
+		public ThymeleafViewResolver thymeleafViewResolver(){
+				final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+				viewResolver.setViewNames(new String[] {"*.html"});
+				viewResolver.setExcludedViewNames(new String[] {"*.jsp"});
+				viewResolver.setTemplateEngine(templateEngine());
+				viewResolver.setCharacterEncoding("UTF-8");
+				return viewResolver;
+		}
+//
+		@Bean
+		public InternalResourceViewResolver jspViewResolver(){
+				final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+				viewResolver.setOrder(10);
+				viewResolver.setViewClass(JstlView.class);
+				viewResolver.setPrefix("/WEB-INF/views/");
+				viewResolver.setSuffix("");
+				viewResolver.setViewNames("*.jsp");
+				return viewResolver;
+		}
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
+
+		@Override
+		public void configureViewResolvers(ViewResolverRegistry registry)
+		{
+				ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+				resolver.setTemplateEngine(templateEngine());
+				registry.viewResolver(resolver);
+		}
+
+		@Bean
+		public DataSource dataSource()
+		{
+				DriverManagerDataSource dataSource = new DriverManagerDataSource();
+				dataSource.setDriverClassName("org.postgresql.Driver");
+				dataSource.setUrl("jdbc:postgresql://localhost:5432/firstDB");
+				dataSource.setUsername("postgres");
+				dataSource.setPassword("123321");
+				return dataSource;
+		}
+
+		@Bean
+		public JdbcTemplate jdbcTemplate()
+		{
+				return new JdbcTemplate(dataSource());
+		}
 
 
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/firstDB");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("123321");
-
-        return  dataSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(){
-        return new JdbcTemplate(dataSource());
-    }
-
+		@Bean(name = "multipartResolver")
+		public CommonsMultipartResolver multipartResolver()
+		{
+				CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+				multipartResolver.setMaxUploadSize(100000);
+				return multipartResolver;
+		}
 }
--- src/main/java/ru/nvn/spring/controllers/PeopleController.java
+++ src/main/java/ru/nvn/spring/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package ru.nvn.spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.nvn.spring.dao.PersonDAO;
+import ru.nvn.spring.models.FormDataWithFile;
 import ru.nvn.spring.models.Person;
 
 import javax.validation.Valid;
@@ -25,13 +28,13 @@ public class PeopleController
 		@GetMapping()
 		public String index(Model model) {
 				model.addAttribute("people", personDAO.index());
-				return "people/index";
+				return "people/index.html";
 		}
 
 		@GetMapping("/{id}")
 		public String show(@PathVariable("id") int id, Model model) {
 				model.addAttribute("person", personDAO.show(id));
-				return "people/show";
+				return "people/show.html";
 		}
 
 		@GetMapping("/new")
@@ -39,11 +42,11 @@ public class PeopleController
 				return "people/new";
 		}
 
-		@PostMapping()
+		@PostMapping("/new")
 		public String create(@ModelAttribute("person") @Valid Person person,
 				BindingResult bindingResult) {
 				if (bindingResult.hasErrors())
-						return "people/new";
+						return "people/new.html";
 
 				personDAO.save(person);
 				return "redirect:/people";
@@ -52,14 +55,14 @@ public class PeopleController
 		@GetMapping("/{id}/edit")
 		public String edit(Model model, @PathVariable("id") int id) {
 				model.addAttribute("person", personDAO.show(id));
-				return "people/edit";
+				return "people/edit.html";
 		}
 
 		@PatchMapping("/{id}")
 		public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
 				@PathVariable("id") int id) {
 				if (bindingResult.hasErrors())
-						return "people/edit";
+						return "people/edit.html";
 
 				personDAO.update(id, person);
 				return "redirect:/people";
@@ -70,4 +73,41 @@ public class PeopleController
 				personDAO.delete(id);
 				return "redirect:/people";
 		}
+
+		////////////////////////////////////////////////////////////////////////////
+		@GetMapping(value = "/fileUpload")
+		public String displayForm(@ModelAttribute("file")  MultipartFile file) {
+				return "people/upload.html";
+		}
+
+		@PostMapping("/fileUpload")
+		public String submit(@ModelAttribute("file")  final MultipartFile file, final ModelMap modelMap) {
+				modelMap.addAttribute("file", file);
+				return "people/fileUploadView.jsp";
+		}
+
+		@PostMapping( "/uploadMultiFile")
+		public String submit(@ModelAttribute("files") final MultipartFile[] files, final ModelMap modelMap) {
+
+				modelMap.addAttribute("files", files);
+				return "people/fileUploadView.jsp";
+		}
+
+		@PostMapping( "/uploadFileWithAddtionalData")
+		public String submit(@ModelAttribute("file") @RequestParam final MultipartFile file, @RequestParam final String name, @RequestParam final String email, final ModelMap modelMap) {
+				modelMap.addAttribute("name", name);
+				modelMap.addAttribute("email", email);
+				modelMap.addAttribute("file", file);
+				return "people/fileUploadView.jsp";
+
+		}
+
+		@PostMapping( "/uploadFileModelAttribute")
+		public String submit(@ModelAttribute final FormDataWithFile formDataWithFile, final ModelMap modelMap) {
+				modelMap.addAttribute("formDataWithFile", formDataWithFile);
+				return "people/fileUploadView.jsp";
+
+		}
+
+
 }
